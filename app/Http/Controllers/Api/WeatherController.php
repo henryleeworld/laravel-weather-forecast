3f8d38341c575ed1f805ff600caec557a7e79368
Henry Lee <henryleeworld@gmail.com>
2021-11-02 11:12:44 +0800

    feat:更新最新資料

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -11,8 +11,10 @@ class WeatherController extends Controller
     public function __invoke($city)
     {
         $coordinates = config('frontend.cities.'.$city);
+
         return Cache::remember('city' . $city, 60 * 5, function() use ($coordinates) {
-            $response = Http::get('https://api.open-meteo.com/v1/forecast?latitude=' . $coordinates['lat'] . '&longitude=' . $coordinates['lng'] . '&daily=temperature_2m_max,temperature_2m_min&timezone=' . config('app.timezone'));
+            $response = Http::get('https://api.open-meteo.com/v1/forecast?latitude='.$coordinates['lat'].'&longitude='.$coordinates['lng'].'&daily=temperature_2m_max,temperature_2m_min&timezone=UTC');
+
             if ($response->successful()) {
                 return $response->json('daily');
             }
