c1fa15ae624693098cf3dbb08cbdaa7949b0f890
Henry Lee <henryleeworld@gmail.com>
2024-03-16 00:12:14 +0800

    feat:更新最新資料

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +14,7 @@ class UserSeeder extends Seeder
             'name' => 'test user',
             'email' => 'test@test.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
         ]);
 
         User::factory(10)->create();
