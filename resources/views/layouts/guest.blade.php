4eac1be29f96fa2e357c119f6e3cd1aafee26cc9
Henry Lee <henryleeworld@gmail.com>
2026-04-26 00:22:48 +0800

    feat:升級至 laravel 13

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
         @vite(['resources/css/app.css'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
